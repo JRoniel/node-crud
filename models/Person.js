4313c0c0af32e8081ed649ef9f72d7d4d03039c4
John Roniel <johnroniel8264@gmail.com>
2024-05-04 19:41:11 -0300

    model alterado para Schema

--- models/Person.js
+++ models/Person.js
@@ -1,9 +1,22 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const Person = mongoose.model('Person', {
-  name: String,
-  salary: Number,
-  approved: Boolean,
-})
+const { Schema, model } = mongoose;
 
-module.exports = Person
+const personSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+  approved: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Person = model('Person', personSchema);
+
+module.exports = Person;
